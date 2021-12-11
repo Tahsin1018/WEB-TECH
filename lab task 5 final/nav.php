69706f1c7a08a9fdcd1fc16b5942b0502abf79a7
Tahsin1018 <65068223+Tahsin1018@users.noreply.github.com>
2021-12-11 13:22:38 +0600

    Delete lab task 5 final directory

--- lab task 5 final/nav.php
+++
@@ -1,5 +0,0 @@
-<ul>
-        <li><a href="addProduct.php"> Add Product</a></li>
-        <li><a href="showAllProducts.php"> Show all Product</a></li>
-        <li><a href="searchProduct.php"> Search Products</a></li>
-    </ul>
